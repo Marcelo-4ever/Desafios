30f7599f3c8a573749d9ddabd2bb3e9be21a1525
Marcelo-Ferreira-Leite <marcelofleitte@gmail.com>
2022-07-13 20:36:13 -0300

    inital commit

--- validar_cpf_regex.py
+++ validar_cpf_regex.py
@@ -3,14 +3,18 @@ import re
 while True: 
     cpf = str(input('Digite seu CPF: ')).strip()
     try:
-        pattern = re.compile(r'\d\d\d\d\d\d\d\d\d\d\d')
+        pattern = re.compile(r'\d\d\d\d\d\d\d\d\d\d\d') 
         find_pattern = pattern.search(cpf)
-        a = find_pattern.group()
+        a = find_pattern.group()  
         if find_pattern.group() and len(cpf) == 11:
             print('CPF válido!')
             break
         print('CPF muito grande!')
     except (AttributeError):
+        pattern = re.compile(r'\d\d\d\.\d\d\d\.\d\d\d-\d\d')
+        find_pattern = pattern.search(cpf)
+        if find_pattern.group() and len(cpf) == 14:
+            print('CPF válido também!')
+            break
         print('Não válido!')
-
 
